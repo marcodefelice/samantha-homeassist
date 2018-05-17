47092045db167a0b906464912e1360f6e7b723e1
Marco De Felice <marco.defelice@icloud.com>
2018-05-17 15:33:45 +0200

    fix an error

--- Controllers/PantryController.js
+++ Controllers/PantryController.js
@@ -149,4 +149,3 @@ function reply(error) {
     }
 
   }
-}
--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var express  = require('express'),
 bodyParser   = require('body-parser'),
 http         = require('http'),
 config       = require('./config'),
-server       = express(),
-mongoose     = require('mongoose'),
+server       = express();
+var mongoose     = require('mongoose')
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect(config.dbUrl);
+
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 var routes = require('./Routes/Routes'); //importing route
