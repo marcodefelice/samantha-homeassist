5fd9a46323de97f8b17cf6f71aeefef0effe454b
Marco De Felice <marco.defelice@icloud.com>
2018-09-06 14:19:14 +0200

    inserito invocazione api

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 module.exports =
 {
  dbUrl:"mongodb://samantha:0335Asia89@ds219000.mlab.com:19000/samantha_db",
- api: {
-   tigros:"",
-   recipes:"",
-   wheather:""
- }
+ api_recipe: "https://api.edamam.com/search?q=chicken&app_id=ddd58851&app_key=2ec894a1ad098641cdba6125ad0722d6&from=0&to=3&calories=591-722&health=alcohol-free"
 }
